5615002a6fae446e8e65bd618269c6c06332132b
백하람 <developer.venice@gmail.com>
2024-03-15 12:00:48 +0900

    feat: mainpage ui

--- const/tripData.js
+++ const/tripData.js
@@ -1,40 +1,70 @@
 export const tripdata = [
   {
     id: 0,
-    imageUrl:
-      "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
     title: "광양 매화 마을",
     location: "전남 광양시",
     like: false,
   },
   {
     id: 1,
-    imageUrl:
-      "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
+    imageUrl: "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
     title: "테미공원",
     location: "대전광역시 중구",
     like: false,
   },
   {
     id: 2,
-    imageUrl:
-      "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
     title: "광양 매화 마을",
     location: "전남 광양시",
     like: false,
   },
   {
     id: 3,
-    imageUrl:
-      "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
+    imageUrl: "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
     title: "테미공원",
     location: "대전광역시 중구",
     like: false,
   },
   {
     id: 4,
-    imageUrl:
-      "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    title: "광양 매화 마을",
+    location: "전남 광양시",
+    like: false,
+  },
+  {
+    id: 5,
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    title: "광양 매화 마을",
+    location: "전남 광양시",
+    like: false,
+  },
+  {
+    id: 6,
+    imageUrl: "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
+    title: "테미공원",
+    location: "대전광역시 중구",
+    like: false,
+  },
+  {
+    id: 7,
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
+    title: "광양 매화 마을",
+    location: "전남 광양시",
+    like: false,
+  },
+  {
+    id: 8,
+    imageUrl: "https://cdn.cctoday.co.kr/news/photo/202104/2134770_571448_3723.jpg",
+    title: "테미공원",
+    location: "대전광역시 중구",
+    like: false,
+  },
+  {
+    id: 9,
+    imageUrl: "https://gwangyang.go.kr/galleryImgView.es?bid=0011&list_no=14471&seq=173",
     title: "광양 매화 마을",
     location: "전남 광양시",
     like: false,
--- src/js/main.js
+++ src/js/main.js
@@ -25,34 +25,54 @@ class MainPage {
       <p>${username}</p>
     `;
     myBtnDiv.innerHTML = myBtn;
-
-    myBtnDiv.addEventListener("click", () => {});
   }
 
   createSearchBar() {
-    const searchDiv = this.#app.getElementById("search");
+    const searchBarDiv = this.#app.getElementById("searchBar");
 
     const searchBar = `
-      <input class="searchbar" placeholder="여행지 이름, 위치 등 검색어를 입력해 주세요." />
+      <input class="search-input" placeholder="여행지 이름, 위치 등 검색어를 입력해 주세요." />
       <img height="30px" width="30px" src="../../assets/images/search.svg" />
     `;
 
-    searchDiv.innerHTML = searchBar;
+    searchBarDiv.innerHTML = searchBar;
   }
 
   createCards() {
     const cardsDiv = this.#app.getElementById("cards");
     let html = "";
+
     tripdata.map((data) => {
-      html += `
-        <div id="card${data.id}" class="card">
-          <img src=${data.imageUrl} />
-          <img src="../../assets/images/empty_star.svg" />
+      html = `
+        <div style="width: 20%" id="card${data.id}" class="card" >
+          <div class="hover">
+            <p>${data.title}</p>
+            <p>${data.location}</p>
+          </div>
+          <img src="../../assets/images/empty_star.svg" alt="star" id="star${data.id}" />
         </div>
       `;
+
+      cardsDiv.innerHTML += html;
+
+      let curCard = document.getElementById(`card${data.id}`);
+      curCard.setAttribute(
+        "style",
+        `width: ${cardsDiv.clientWidth / 5 - 10}px; height: ${cardsDiv.clientWidth / 5 - 10}px; 
+        background-image: url(${data.imageUrl}); background-size: cover`
+      );
     });
 
-    cardsDiv.innerHTML = html;
+    this.starClick();
+  }
+
+  starClick() {
+    for (let i = 0; i < tripdata.length; i++) {
+      let starIcon = this.#app.getElementById(`star${tripdata[i].id}`);
+      starIcon.addEventListener("click", () => {
+        starIcon.setAttribute("src", "../../assets/images/full_star.svg");
+      });
+    }
   }
 }
 
